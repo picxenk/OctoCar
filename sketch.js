3d22d94bade3cd96405f6646a101d14efdf0a8f5
Seungbum Kim <picxenk@gmail.com>
2022-10-25 16:07:39 +0900

    add example

--- sketch.js
+++ sketch.js
@@ -1,8 +1,18 @@
+let ball;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400, 200);
+
+  ball = new Particle();
 }
+
 
 function draw() {
   background(220);
-  circle(width/2, height/2, 80);
+
+  let gravity = createVector(0, 0.1);
+  ball.addForce(gravity);
+
+  ball.update();
+  ball.show();
 }
